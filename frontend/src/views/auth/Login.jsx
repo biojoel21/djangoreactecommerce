06f16e7413dab7937fe0655dfc8733c8906da19d
biojoel21 <eebc_5@hotmail.com>
2024-10-25 12:14:37 -0400

    comenzar seccion 28 y vamos por el minuto 20

---
+++ frontend/src/views/auth/Login.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import { login } from '../../utils/auth'
+import { useNavigate, Link } from 'react-router-dom'
+import { useAuthStore } from '../../store/auth'
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
