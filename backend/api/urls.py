dc96ce4c1277c0f2ad0c3dba3f9915ead2614a90
biojoel21 <eebc_5@hotmail.com>
2024-11-07 11:45:59 -0400

    terminar punto 31

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('user/token/', userauths_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/token/refresh/', TokenRefreshView.as_view()),
     path('user/register/', userauths_views.RegisterView.as_view(), name='register'),
+    path('user/password-reset/<email>', userauths_views.PasswordResetEmailVerify.as_view()),
 ]
